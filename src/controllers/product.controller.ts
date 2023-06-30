fcb4ba9af44ee152bd82858476c75e4af2151fc3
snoopysaurav <saurav2k60@gmail.com>
2023-06-30 23:15:49 +0545

    feat: dummy changes on product 🚀

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from "express";
 import { FileRequest } from "../middlewares/file.middleware";
+import { AppDatasource } from "../database/datasource";
+import ProductEntity from "../database/product.entity";
+
+const ProductRepository = AppDatasource.getRepository(ProductEntity);
 
 const addProduct = async (req: Request, res: Response) => {
   return res.status(200).json({ msg: `Product Added` });
 };
+
+const getAllProduct = async (req:Request, res:Response)=>{
+  const product = ProductRepository.find();
+  return res.status(200).json({product});
+}
 
 const uploadProductImage = async (req: FileRequest, res: Response) => {
   console.log(req.files);
   return res.status(200).json({ FileName: req.files });
 };
 
-export { uploadProductImage };
+export { getAllProduct, uploadProductImage };
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import fileMiddleware from "../middlewares/file.middleware";
-import { uploadProductImage } from "../controllers/product.controller";
+import { getAllProduct, uploadProductImage } from "../controllers/product.controller";
+import authMiddleware from "../middlewares/auth.middleware";
 
 const productRoute: Router = Router();
+
+productRoute.route("/product").get( authMiddleware,getAllProduct);
 
 productRoute.route("/api/uploads").post(fileMiddleware, uploadProductImage);
 
